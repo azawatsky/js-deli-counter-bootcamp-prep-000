77ece51f406e6a1a92fc37515e6a435f48d3083d
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-06-01 16:58:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(currentLine,newPerson) {
-  currentLine=currentLine.push(newPerson)
-  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
+function takeANumber(katzDeliLine,newPerson) {
+  katzDeliLine=katzDeliLine.push(newPerson)
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
 }
